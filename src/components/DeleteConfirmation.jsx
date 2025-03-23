e7f2ea7c0d43957451f16cac39b066100e1b18eb
Shohrukh Koyirov <shohrukhkoyirov@gmail.com>
2025-03-23 10:40:38 +0100

    removing by timer added through useEffect

--- src/components/DeleteConfirmation.jsx
+++ src/components/DeleteConfirmation.jsx
@@ -1,5 +1,18 @@
+import { useEffect } from "react";
+
 export default function DeleteConfirmation({ onConfirm, onCancel }) {
-  return (
+
+    useEffect(() => {
+        const timout = setTimeout(() => {
+            onConfirm();
+        }, 3000);
+
+        return () => {
+            clearTimeout(timout);
+        }
+    }, [])
+
+    return (
     <div id="delete-confirmation">
       <h2>Are you sure?</h2>
       <p>Do you really want to remove this place?</p>
